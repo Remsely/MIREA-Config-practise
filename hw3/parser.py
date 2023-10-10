55eb6918739a0abf461fd88b9dafbc92dcf186d7
Remsely <prokopchukromain@mail.ru>
2023-10-11 00:58:11 +0300

    practise3 final

--- hw3/parser.py
+++ hw3/parser.py
@@ -1,6 +1,9 @@
-import ply.lex as lex
-from ply import yacc
+import os
+import sys
 import json
+from ply import lex
+from ply import yacc
+
 
 tokens = (
     'NUMBER',
@@ -91,43 +94,17 @@ def p_error(p):
 
 parser = yacc.yacc()
 
-data = '''
-(groups
-    ("ИКБО-1-20"
-    "ИКБО-2-20"
-    "ИКБО-3-20"
-    "ИКБО-4-20"
-    "ИКБО-5-20"
-    "ИКБО-6-20"
-    "ИКБО-7-20"
-    "ИКБО-8-20"
-    "ИКБО-9-20"
-    "ИКБО-10-20"
-    "ИКБО-11-20"
-    "ИКБО-12-20"
-    "ИКБО-13-20"
-    "ИКБО-14-20"
-    "ИКБО-15-20"
-    "ИКБО-16-20"
-    "ИКБО-16-20"
-    "ИКБО-17-20"
-    "ИКБО-18-20"
-    "ИКБО-19-20"
-    "ИКБО-20-20"
-    "ИКБО-21-20"
-    "ИКБО-22-20"
-    "ИКБО-23-20"
-    "ИКБО-24-20")
-students (
-(age 19 group "ИКБО-4-20" name "Иванов И.И.")
-(age 18 group "ИКБО-5-20" name "Петров П.П.")
-(age 18 group "ИКБО-5-20" name "Сидоров С.С.")
-(age 18 group "ИКБО-1-22" name "Прокопчук Р.О."))
-subject "Конфигурационное управление")
-'''
+if len(sys.argv) != 2:
+    print("Usage: python your_script.py input_file")
+    sys.exit(1)
+
+input_file = sys.argv[1]
+current_directory = os.getcwd()
+file_path = os.path.join(current_directory, input_file)
+
+data = ''.join(open(file_path, 'r', encoding='UTF-8').readlines())
 
 result = parser.parse(data)
-
 # print(result)
 json_result = json.dumps(result[0], ensure_ascii=False, indent=2)
 print(json_result)
